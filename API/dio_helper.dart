1076e242d5b8fd8724a72b20a121dc049e983a11
AnasDarwesh20 <114184224+AnasDarwesh20@users.noreply.github.com>
2022-12-08 14:56:17 +0200

    Add files via upload

---
+++ API/dio_helper.dart
@@ -0,0 +1,56 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+
+class DioHelper
+{
+  static Dio  dio  ;
+
+  static init ()
+  {
+      dio = Dio(
+      BaseOptions(
+        baseUrl: 'https://student.valuxapps.com/api/' ,
+        receiveDataWhenStatusError: true ,
+      ) ,
+    ) ;
+  }
+
+  static Future<Response> getData({
+  @required String url ,
+    Map <String , dynamic> query ,
+    String token ,
+    String lang = 'en' ,
+
+}) async
+  {
+    dio.options.headers =
+    {
+      'lang' : lang ,
+      'Authorization' : token??'' ,
+      'Content-Type' : 'application/jason',
+    }  ;
+    return await dio.get(
+      url ,
+      queryParameters: query ,
+    ) ;
+  }
+
+  static Future<Response> postData ({
+    @required String url ,
+    @required Map <String , dynamic> data ,
+    dynamic token ,
+    String lang = 'en' ,
+})async
+  {
+    dio.options.headers=
+    {
+      'lang': lang ,
+      'Authorization': token?? '' ,
+      'Content-Type' : 'application/jason',
+    };
+    return dio.post(
+      url ,
+      data: data,
+    ) ;
+}
+}
